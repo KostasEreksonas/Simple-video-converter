5a252629bcf012134eee6489a048fed2342a6beb
KostasEreksonas <k.ereksonas@gmail.com>
2022-09-25 20:22:10 +0300

    Added a script for video conversion

---
+++ file_converter.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+read -p "Enter the file format that you want to convert: " format
+
+# Find all files in current working directory with entered file format
+# Solution found at: https://stackoverflow.com/questions/23356779/how-can-i-store-the-find-command-results-as-an-array-in-bash
+readarray -d '' videos < <(find . -type f -name "*.$format" -print0)
+
+# Set arguments for ffmpeg video conversion
+read -p "Choose ffmpeg preset: " preset
+read -p "Choose CRF value (0-53): " crf
+
+# Iterate trough array and convert all video files to mp4
+len=${#videos[@]}
+for (( i=0; i<$len; i++ )); do
+	# Create a name for converted file
+	output=$(echo ${videos[$i]} | sed 's/.mkv/_conv.mp4/g')
+
+	# Convert given video file
+	ffmpeg -i ${videos[$i]} -c:a copy -c:v libx264 -preset $preset -crf $crf $output
+done

257d30b574f02ef46364a3d7335dc200ee65622f
KostasEreksonas <k.ereksonas@gmail.com>
2022-09-26 10:58:30 +0300

    Omit hidden folders from search

--- file_converter.sh
+++ file_converter.sh
@@ -5,7 +5,7 @@ while true; do
 
 	# Add all found videos with selected format to videos array
 	# Solution found at: https://stackoverflow.com/questions/23356779/how-can-i-store-the-find-command-results-as-an-array-in-bash
-	readarray -d '' -O "${#videos[@]}" videos < <(find . -type f -name "*.$format" -print0)
+	readarray -d '' -O "${#videos[@]}" videos < <(find . -not -path "*/.*" -type f -name "*".$format -print0)
 	read -p "Do you want to add another file format to convert? (Y/N) " choice
 	if [ $choice == "N" ]; then
 		break
@@ -21,9 +21,10 @@ read -p "Choose CRF value (0-53): " crf
 # Iterate trough array and convert all video files to mp4
 len=${#videos[@]}
 for (( i=0; i<$len; i++ )); do
+	printf "${videos[$i]}\n"
 	# Update file extension of converted file
-	output=$(echo ${videos[$i]} | sed "s/.$format/_conv.mp4/g")
+	#output=$(echo ${videos[$i]} | sed "s/.$format/_conv.mp4/g")
 
 	# Convert given video file
-	ffmpeg -i ${videos[$i]} -c:a $audio -c:v $video -preset $preset -crf $crf $output
+	#ffmpeg -i ${videos[$i]} -c:a $audio -c:v $video -preset $preset -crf $crf $output
 done

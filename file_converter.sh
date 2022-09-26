16ea014c64c564af22dcb85092e7108ee4508613
KostasEreksonas <k.ereksonas@gmail.com>
2022-09-26 10:17:36 +0300

    Added an option to choose multiple video file extensions to convert to mp4

--- file_converter.sh
+++ file_converter.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
-read -p "Enter the file format that you want to convert: " format
+while true; do
+	read -p "Enter the file format that you want to convert: " format
 
-# Find all files in the current working directory with selected file format
-# Solution found at: https://stackoverflow.com/questions/23356779/how-can-i-store-the-find-command-results-as-an-array-in-bash
-readarray -d '' videos < <(find . -type f -name "*.$format" -print0)
+	# Add all found videos with selected format to videos array
+	# Solution found at: https://stackoverflow.com/questions/23356779/how-can-i-store-the-find-command-results-as-an-array-in-bash
+	readarray -d '' -O "${#videos[@]}" videos < <(find . -type f -name "*.$format" -print0)
+	read -p "Do you want to add another file format to convert? (Y/N) " choice
+	if [ $choice == "N" ]; then
+		break
+	fi
+done
 
 # Set arguments for ffmpeg video conversion
 read -p "Choose audio settings: " audio
@@ -15,9 +21,10 @@ read -p "Choose CRF value (0-53): " crf
 # Iterate trough array and convert all video files to mp4
 len=${#videos[@]}
 for (( i=0; i<$len; i++ )); do
+	printf "${videos[$i]}\n"
 	# Update file extension of converted file
-	output=$(echo ${videos[$i]} | sed "s/.$format/_conv.mp4/g")
+	#output=$(echo ${videos[$i]} | sed "s/.$format/_conv.mp4/g")
 
 	# Convert given video file
-	ffmpeg -i ${videos[$i]} -c:a $audio -c:v $video -preset $preset -crf $crf $output
+	#ffmpeg -i ${videos[$i]} -c:a $audio -c:v $video -preset $preset -crf $crf $output
 done

9e1333c1a5f0a0f6d4657238c5c05220ce0932c1
KostasEreksonas <k.ereksonas@gmail.com>
2022-12-12 21:18:26 +0200

    Check wether CRF is between 0 - 53

--- file_converter_ncurses.sh
+++ file_converter_ncurses.sh
@@ -11,14 +11,20 @@ while true; do
 done
 
 # Set ffmpeg video conversion arguments and their defaults
-audio=$(dialog --stdout --title "Choose Audio Settings" --inputbox "Enter settings for audio" 0 0)
+audio=$(dialog --stdout --title "Choose Audio Settings" --inputbox "Enter settings for audio (default: copy)" 0 0)
 if [ -z $audio ]; then audio="${1:-copy}"; fi
-video=$(dialog --stdout --title "Choose Video Settings" --inputbox "Enter settings for video" 0 0)
+
+video=$(dialog --stdout --title "Choose Video Settings" --inputbox "Enter settings for video (default: libx264)" 0 0)
 if [ -z $video ]; then video="${1:-libx264}"; fi
-preset=$(dialog --stdout --title "Choose Preset" --inputbox "Choose preset" 0 0)
+
+preset=$(dialog --stdout --title "Choose Preset" --inputbox "Choose preset (default: medium)" 0 0)
 if [ -z $preset ]; then preset="${1:-medium}"; fi
-crf=$(dialog --stdout --title "Choose CRF Value" --inputbox "Choose CRF value" 0 0)
-if [ -z $crf ]; then crf="${1:-17}"; fi
+
+# CRF must be between 0 - 53
+while ! [[ $crf =~ ^[0-9]*$ && $crf -ge 0 && $crf -le 53 ]]; do
+	crf=$(dialog --stdout --title "Choose CRF Value" --inputbox "Choose CRF value (must be between 0 - 53, default: 17)" 0 0)
+	if [ -z $crf ]; then crf="${1:-17}"; fi
+done
 
 # Convert all given video files to mp4
 len=${#videos[@]}
@@ -29,6 +35,6 @@ for (( i=0; i<$len; i++ )); do
 	# Convert a given video file
 	ffmpeg -i ${videos[$i]} -c:a $audio -c:v $video -preset $preset -crf $crf $output
 
-	dialog --title "Keeping original video file" --yesno "Do you want to keep original file?" 0 0
+#	dialog --title "Keeping original video file" --yesno "Do you want to keep original file?" 0 0
 	if [ $? == 1 ]; then rm ${videos[$i]}; fi
 done

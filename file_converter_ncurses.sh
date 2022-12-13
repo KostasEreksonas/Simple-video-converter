1004da7cfdeb941a798699009363c42fc76536e9
KostasEreksonas <k.ereksonas@gmail.com>
2022-12-13 17:04:59 +0200

    Check whether selected settings are correct

--- file_converter_ncurses.sh
+++ file_converter_ncurses.sh
@@ -10,21 +10,32 @@ while true; do
 	if [ $? == 1 ]; then break; fi
 done
 
-# Set ffmpeg video conversion arguments and their defaults
-audio=$(dialog --stdout --title "Choose Audio Settings" --inputbox "Enter settings for audio (default: copy)" 0 0)
-if [ -z $audio ]; then audio="${1:-copy}"; fi
+# Restart a sequence of settings prompts if some are incorrect
+check=1
+while [ $check -eq 1 ]; do
+	# Set ffmpeg video conversion arguments and their defaults
+	audio=$(dialog --stdout --title "Choose Audio Settings" --inputbox "Enter settings for audio (default: copy)" 0 0)
+	if [ -z $audio ]; then audio="${1:-copy}"; fi
 
-video=$(dialog --stdout --title "Choose Video Settings" --inputbox "Enter settings for video (default: libx264)" 0 0)
-if [ -z $video ]; then video="${1:-libx264}"; fi
+	video=$(dialog --stdout --title "Choose Video Settings" --inputbox "Enter settings for video (default: libx264)" 0 0)
+	if [ -z $video ]; then video="${1:-libx264}"; fi
 
-preset=$(dialog --stdout --title "Choose Preset" --inputbox "Choose preset (default: medium)" 0 0)
-if [ -z $preset ]; then preset="${1:-medium}"; fi
+	preset=$(dialog --stdout --title "Choose Preset" --inputbox "Choose preset (default: medium)" 0 0)
+	if [ -z $preset ]; then preset="${1:-medium}"; fi
 
-# CRF must be between 0 - 53
-crf=-1
-while ! [[ $crf =~ ^[0-9]*$ && $crf -ge 0 && $crf -le 53 ]]; do
-	crf=$(dialog --stdout --title "Choose CRF Value" --inputbox "Choose CRF value (must be between 0 - 53, default: 17)" 0 0)
-	if [ -z $crf ]; then crf="${1:-17}"; fi
+	# CRF must be between 0 - 53
+	crf=-1
+	while ! [[ $crf =~ ^[0-9]*$ && $crf -ge 0 && $crf -le 53 ]]; do
+		crf=$(dialog --stdout --title "Choose CRF Value" \
+			--inputbox "Choose CRF value (between 0 - 53, default: 17)" 0 0)
+		if [ -z $crf ]; then crf="${1:-17}"; fi
+	done
+
+	dialog --title "Settings" --yesno "Please confirm your settings:\n\
+	Audio: $audio\nVideo: $video\nPreset: $preset\nCRF: $crf\n\
+	Would you like to continue with these settings? (Yes/No)" 0 0
+
+	check=$? && if [ $check -eq 0 ]; then break; fi
 done
 
 # Convert all given video files to mp4
@@ -36,6 +47,6 @@ for (( i=0; i<$len; i++ )); do
 	# Convert a given video file
 	ffmpeg -i ${videos[$i]} -c:a $audio -c:v $video -preset $preset -crf $crf $output
 
-#	dialog --title "Keeping original video file" --yesno "Do you want to keep original file?" 0 0
+	dialog --title "Keeping original video file" --yesno "Do you want to keep original file?" 0 0
 	if [ $? == 1 ]; then rm ${videos[$i]}; fi
 done
